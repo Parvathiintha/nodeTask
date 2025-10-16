b9d9b4994340b7badca255acc32e30e087d0a571
Parvathiintha <parvathi.intha.com>
2025-10-16 15:26:50 +0530

    change url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-uiswagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/users", authRouter);
 
 connectDB().then(() => {
